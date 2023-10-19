6ecbb2060bfdf286baff4b2070df24f8c107ff62
Srinath Srinivasan <ssrini27@ncsu.edu>
2023-10-19 10:06:51 -0400

    fixing indents in test_edit

--- test/test_edit.py
+++ test/test_edit.py
@@ -11,21 +11,13 @@ class EditTests(unittest.TestCase):
         message = Mock()
         message.chat.id = 123
         bot = Mock()
-
-     
         MockHelper.getUserHistory.return_value = ['2023-10-10,food,10']
-        
         markup_instance = Mock()
         MockReplyKeyboardMarkup.return_value = markup_instance
-
-     
         edit.run(message, bot)
-
         markup_instance.add.assert_called_with("Date=2023-10-10,\t\tCategory=food,\t\tAmount=$10")
         bot.reply_to.assert_called_once()
         bot.register_next_step_handler.assert_called_once_with(bot.reply_to.return_value, edit.select_category_to_be_updated, bot)
-    
-
 
     @patch('edit.helper')
     def test_edit_date(self, MockHelper):
@@ -35,17 +27,14 @@ class EditTests(unittest.TestCase):
         result = "2023-10-11"
         chat_id = 123
         
-        
         MockHelper.getUserHistory.return_value = ['2023-10-10,food,10']
         MockHelper.read_json.return_value = {str(chat_id): {"data": ['2023-10-10,food,10']}}
         
-
         edit.edit_date(bot, selected_data, result, chat_id)
         
- 
         MockHelper.write_json.assert_called_once()
-   
-    class EditTests(unittest.TestCase):
+
+class EditTests(unittest.TestCase):
     # Previous test cases...
 
     @patch('edit.helper')
@@ -55,14 +44,12 @@ class EditTests(unittest.TestCase):
         message = Mock()
         message.text = "Date=2023-10-10, Category=food, Amount=$10"
         bot = Mock()
-        
+
         markup_instance = Mock()
         MockReplyKeyboardMarkup.return_value = markup_instance
         
-   
         edit.select_category_to_be_updated(message, bot)
         
-
         markup_instance.add.assert_called_with("Date=2023-10-10")
         bot.reply_to.assert_called_once()
         bot.register_next_step_handler.assert_called_once_with(bot.reply_to.return_value, edit.enter_updated_data, bot, message.text.split(","))
@@ -77,33 +64,28 @@ class EditTests(unittest.TestCase):
         
         selected_data = ["Date=2023-10-10", "Category=food", "Amount=$10"]
         
-
         MockHelper.getUserHistory.return_value = ['2023-10-10,food,10']
         MockHelper.validate_entered_amount.return_value = 1
         
-  
         edit.edit_cost(message, bot, selected_data)
         
-     
         MockHelper.write_json.assert_called_once()
         bot.reply_to.assert_called_once_with(message, "Expense amount is updated")
 
     @patch('edit.helper')
     def test_edit_cost_with_invalid_input(self, MockHelper):
-       
         message = Mock()
         message.text = "invalid_input"
         message.chat.id = 123
         bot = Mock()
         
         selected_data = ["Date=2023-10-10", "Category=food", "Amount=$10"]
-       
+    
         MockHelper.getUserHistory.return_value = ['2023-10-10,food,10']
         MockHelper.validate_entered_amount.return_value = 0
-      
+    
         edit.edit_cost(message, bot, selected_data)
         
-    
         MockHelper.write_json.assert_not_called()
         bot.reply_to.assert_called_once_with(message, "The cost is invalid")
 
@@ -117,16 +99,12 @@ class EditTests(unittest.TestCase):
         
         selected_data = ["Date=2023-10-10", "Category=food", "Amount=$10"]
         
-      
         MockHelper.getUserHistory.return_value = ['2023-10-10,food,10']
         
-     
         edit.edit_cat(message, bot, selected_data)
-        
         
         MockHelper.write_json.assert_called_once()
         bot.reply_to.assert_called_once_with(message, "Category is updated")
-
 
 if __name__ == '__main__':
     unittest.main()
